c14a106cc21f573e8982a6be2c466152de4e931a
PhucLH-dev <phuclh8604@gmail.com>
2024-10-09 00:06:55 +0700

    M

--- font-end/pethouse/src/App.js
+++ font-end/pethouse/src/App.js
@@ -3,12 +3,17 @@ import Slider from "./slider";
 import Product from "./product";
 import Product_hot from "./product-hot";
 import Big_sale from "./big_sale";
+import Footer from "./footer";
 function App() {
   return (
     <div class="boxed">
       {/* header */}
       <div id="site-header-wrap">
-        <Header />
+        <header id="header" class="header header-container clearfix">
+        <div class="container clearfix" id="site-header-inner">
+          <Header/>
+        </div>
+        </header>
       </div>
       {/* header */}
       <Slider />
@@ -19,14 +24,20 @@ function App() {
       </section>
       <section class="flat-row row-product-new">
         <div class="container">
-          <Product_hot/>
+          <Product_hot />
         </div>
       </section>
       <section class="flat-row row-animation-box bg-section row-1">
         <div class="container">
-          <Big_sale/>
+          <Big_sale />
         </div>
       </section>
+      {/* Footer */}
+      <footer class="footer">
+        <div class="container">
+          <Footer />
+        </div>
+      </footer>
     </div>
   );
 }
