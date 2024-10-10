b5dd2b6b290ac2ad2699bd6d5b26511a8a49657f
PhucLH-dev <phuclh8604@gmail.com>
2024-10-10 21:46:02 +0700

    Phuc

--- font-end/pethouse/src/App.js
+++ font-end/pethouse/src/App.js
@@ -1,9 +1,5 @@
 import Header from "./header";
-import Slider from "./slider";
-import Product from "./product";
-import Product_hot from "./product-hot";
-import Big_sale from "./big_sale";
-import Best_sale from "./best_sale";
+import Index from "./component";
 import Footer from "./footer";
 function App() {
   return (
@@ -17,27 +13,7 @@ function App() {
         </header>
       </div>
       {/* header */}
-      <Slider />
-      <section class="flat-row row-image-box">
-        <div class="container">
-          <Product />
-        </div>
-      </section>
-      <section class="flat-row row-product-new">
-        <div class="container">
-          <Product_hot />
-        </div>
-      </section>
-      <section class="flat-row row-animation-box bg-section row-1">
-        <div class="container">
-          <Big_sale />
-        </div>
-      </section>
-      <section class="flat-row row-product-project style-1">
-        <div class="container">
-          <Best_sale />
-        </div>
-      </section>
+      <Index />
       {/* Footer */}
       <footer class="footer">
         <div class="container">
