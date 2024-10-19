b4e98f5473aa47fc99bb2951c70b3065d8b22770
TThanhTu-176 <tranthanhtu170620042012@gmail.com>
2024-10-19 11:17:09 +0700

    Tu

--- font-end/pethouse/src/App.js
+++ font-end/pethouse/src/App.js
@@ -2,13 +2,13 @@ import Header from "./header";
 import Index from "./component";
 import Footer from "./footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import TinTuc from "./component/tintuc";
 import LienHe from "./component/lienhe";
-<<<<<<< HEAD
 import ChiTietTinTuc from "./component/chitiettintuc";
 import SanPham from "./component/sanpham";
 import ChiTietSanPham from "./component/chitietsanpham";
-=======
+
 import Admin_TrangChu from "./Admin_TrangChu";
 import Admin_TaiKhoan from "./Admin_TaiKhoan";
 import Admin_TaiKhoanThem from "./Admin_TaiKhoanThem";
@@ -16,7 +16,6 @@ import Admin_TaiKhoanSua from "./Admin_TaiKhoanSua";
 import Admin_SanPham from "./Admin_SanPham";
 import Admin_SanPhamChiTiet from "./Admin_SanPhamChiTiet";
 
->>>>>>> f6a75f435641cfa6b353169a8f2272b5bd17d698
 function App() {
   return (
     <BrowserRouter>
@@ -43,27 +42,6 @@ function App() {
                 </div>
               </footer>
             </div>
-<<<<<<< HEAD
-          </header>
-        </div>
-        {/* header */}
-        <Routes>
-          <Route path='/' exact element={<Index />} />
-          <Route path='/tintuc' exact element={<TinTuc/>} />
-          <Route path='/lienhe' exact element={<LienHe/>} />
-          <Route path='/sanpham' exact element={<SanPham/>} />
-          <Route path='/chitietsanpham' exact element={<ChiTietSanPham/>} />
-          <Route path='/chitiettintuc' exact element={<ChiTietTinTuc/>} />
-        </Routes>
-
-        {/* Footer */}
-        <footer class="footer">
-          <div class="container">
-            <Footer />
-          </div>
-        </footer>
-      </div>
-=======
           }
         />
         <Route
@@ -112,6 +90,75 @@ function App() {
             </div>
           }
         />
+        <Route
+          path="/sanpham"
+          exact
+          element={
+            <div class="boxed">
+              {/* header */}
+              <div id="site-header-wrap">
+                <header id="header" class="header header-container clearfix">
+                  <div class="container clearfix" id="site-header-inner">
+                    <Header />
+                  </div>
+                </header>
+              </div>
+              <SanPham />
+              {/* Footer */}
+              <footer class="footer">
+                <div class="container">
+                  <Footer />
+                </div>
+              </footer>
+            </div>
+          }
+        />
+        <Route
+          path="/chitiettintuc"
+          exact
+          element={
+            <div class="boxed">
+              {/* header */}
+              <div id="site-header-wrap">
+                <header id="header" class="header header-container clearfix">
+                  <div class="container clearfix" id="site-header-inner">
+                    <Header />
+                  </div>
+                </header>
+              </div>
+              <ChiTietTinTuc />
+              {/* Footer */}
+              <footer class="footer">
+                <div class="container">
+                  <Footer />
+                </div>
+              </footer>
+            </div>
+          }
+        />
+        <Route
+          path="/chitietsanpham"
+          exact
+          element={
+            <div class="boxed">
+              {/* header */}
+              <div id="site-header-wrap">
+                <header id="header" class="header header-container clearfix">
+                  <div class="container clearfix" id="site-header-inner">
+                    <Header />
+                  </div>
+                </header>
+              </div>
+              <ChiTietSanPham />
+              {/* Footer */}
+              <footer class="footer">
+                <div class="container">
+                  <Footer />
+                </div>
+              </footer>
+            </div>
+          }
+        />
         <Route path="/admin" exact element={<Admin_TrangChu />} />
         <Route path="/admintaikhoan" exact element={<Admin_TaiKhoan />} />
         <Route
@@ -121,9 +168,12 @@ function App() {
         />
         <Route path="/admintaikhoansua" exact element={<Admin_TaiKhoanSua />} />
         <Route path="/adminsanpham" exact element={<Admin_SanPham />} />
-        <Route path="/adminsanphamchitiet" exact element={<Admin_SanPhamChiTiet />} />
+        <Route
+          path="/adminsanphamchitiet"
+          exact
+          element={<Admin_SanPhamChiTiet />}
+        />
       </Routes>
->>>>>>> f6a75f435641cfa6b353169a8f2272b5bd17d698
     </BrowserRouter>
   );
 }
