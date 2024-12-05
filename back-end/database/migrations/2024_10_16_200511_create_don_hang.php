e111abcef28d31780d6d4d8951da0da632ec89b5
TThanhTu-176 <tranthanhtu170620042012@gmail.com>
2024-12-05 14:10:13 +0700

    Đang cập nhật admin thêm đơn hàng và CSDL

--- back-end/database/migrations/2024_10_16_200511_create_don_hang.php
+++ back-end/database/migrations/2024_10_16_200511_create_don_hang.php
@@ -14,16 +14,16 @@ return new class extends Migration
         Schema::create('don_hang', function (Blueprint $table) {
             $table->id('MaDH'); // Khóa chính
             $table->unsignedBigInteger('Mataikhoan'); // Khóa ngoại đến bảng TaiKhoan
-            $table->decimal('TongTien', 10, 2); 
+            $table->decimal('TongTien', 10, 2);
             $table->decimal('Discount', 10, 2)->default(0); // Thêm trường Discount với kiểu dữ liệu phù hợp
             $table->integer('SoLuong');
             $table->string('Ten'); // Có thể là tên người nhận
             $table->string('SDT');
-            $table->string('DiaChi');
+            $table->string('DiaChi')->nullable();
             $table->string('PTTT'); // Phương thức thanh toán
             $table->string('GhiChu')->nullable();
             $table->string('Loai');
-            $table->enum('TrangThai', ['cho_xac_nhan','da_xac_nhan', 'dang_van_chuyen', 'da_thanh_toan', 'hoan_thanh', 'huy'])->default('cho_xac_nhan'); // Trạng thái đơn hàng
+            $table->enum('TrangThai', ['cho_xac_nhan', 'da_xac_nhan', 'dang_van_chuyen', 'da_thanh_toan', 'hoan_thanh', 'huy'])->default('cho_xac_nhan'); // Trạng thái đơn hàng
             $table->dateTime('NgayDat')->nullable();  // Ngày và giờ đặt hàng có thể để trống
             $table->dateTime('NgayGiao')->nullable(); // Ngày và giờ giao hàng có thể để trống
 
